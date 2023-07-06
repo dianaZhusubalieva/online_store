a4db9a6ae8c03ec6ed5a4ed45e3e4fd814569e79
diana <dinazhusubalieva@gmail.com>
2023-07-06 15:34:12 +0600

    add authMiddleware, checkRoleMiddleware, device api, etc

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -1,12 +1,12 @@
 const uuid = require("uuid");
-const path = require("path ");
-const { Device } = require("./../models/models");
+const path = require("path");
+const { Device, DeviceInfo } = require("./../models/models");
 const ApiError = require("./../error/ApiError");
 
 class DeviceController {
     async create(req, res, next) {
         try {
-            const { name, price, brandId, typeId, info } = req.body;
+            let { name, price, brandId, typeId, info } = req.body;
 
             const { img } = req.files;
             let fileName = uuid.v4() + ".jpg";
@@ -20,15 +20,55 @@ class DeviceController {
                 img: fileName,
             });
 
+            if (info) {
+                info = JSON.parse(info);
+                info.forEach((i) =>
+                    DeviceInfo.create({
+                        title: i.title,
+                        description: i.description,
+                        deviceId: device.id,
+                    })
+                );
+            }
             return res.json(device);
         } catch (e) {
             next(ApiError.badRequest(e.message));
         }
     }
 
-    async getAll(req, res) {}
+    async getAll(req, res) {
+        let { brandId, typeId, page = 1, limit = 9 } = req.query;
+        // page = page || 1;
+        // limit = limit || 9;
 
-    async getOne(req, res) {}
+        ////depending on current page how many items we should skip from previous
+        let offset = page * limit - limit;
+        let devices;
+        if (!brandId && !typeId) {
+            devices = await Device.findAndCountAll({ limit, offset });
+            console.log("devices", devices);
+        }
+
+        if (brandId && !typeId) {
+            devices = await Device.findAndCountAll({ where: { brandId }, limit, offset });
+        }
+
+        if (!brandId && typeId) {
+            devices = await Device.findAndCountAll({ where: { typeId }, limit, offset });
+        }
+
+        if (brandId && typeId) {
+            devices = await Device.findAndCountAll({ where: { brandId, typeId }, limit, offset });
+        }
+        return res.json(devices);
+    }
+
+    async getOne(req, res) {
+        const { id } = req.params;
+        const foundDevice = await Device.findOne({ where: { id }, include: [{ model: DeviceInfo, as: "info" }] });
+
+        return res.json(foundDevice);
+    }
 }
 
 module.exports = new DeviceController();
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,58 @@
 const ApiError = require("./../error/ApiError");
+const bcrypt = require("bcrypt");
+const { User, Basket } = require("./../models/models");
+const jwt = require("jsonwebtoken");
+
+const generateJwt = (id, email, role) =>
+    jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
+        expiresIn: "24h",
+    });
 
 class UserController {
-    async registration(req, res) {}
-
-    async login(req, res) {}
-    async check(req, res, next) {
-        const { id } = req.query;
-
-        if (!id) {
-            return next(ApiError.badRequest("no id provided"));
+    async registration(req, res, next) {
+        const { email, password, role } = req.body;
+        if (!email?.length || !password?.length) {
+            return next(ApiError.badRequest("Некорректный email или пароль"));
         }
-        res.json(id);
+
+        const isAlreadySigned = await User.findOne({ where: { email } });
+
+        if (isAlreadySigned) {
+            next(ApiError.badRequest("Пользователь с таким email уже существует"));
+        }
+
+        const hashPassword = await bcrypt.hash(password, 5);
+        const user = await User.create({ email, password: hashPassword, role });
+        const basket = await Basket.create({ userId: user.id });
+
+        ////first arg {} - payload of token
+        const token = generateJwt(user.id, user.email, user.role);
+        return res.json({ token });
+    }
+
+    async login(req, res, next) {
+        const { email, password } = req.body;
+        const user = await User.findOne({
+            where: {
+                email,
+            },
+        });
+        if (!user) {
+            return next(ApiError.internalServer("User not found"));
+        }
+
+        let isPasswordEqual = bcrypt.compareSync(password, user.password);
+        if (!isPasswordEqual) {
+            return next(ApiError.internalServer("Incorrect email or password"));
+        }
+        const token = generateJwt(user.id, user.email, user.role);
+        res.json({ token });
+    }
+
+    async check(req, res, next) {
+        const { email, id, role } = req.user;
+        const token = generateJwt(id, email, role);
+        return res.json({ token });
     }
 }
 
